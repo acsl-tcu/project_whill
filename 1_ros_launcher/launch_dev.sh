5e0bf29635c48a900963307f2fc8c9350511672b
kasekiguchi <kasekiguchi@gmail.com>
2024-12-20 13:59:19 +0900

    整理

--- 1_ros_launcher/launch_dev.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-echo "nothing launched"
-# cd /root/ros2_ws
-# colcon build --symlink-install
-# source install/setup.bash
-#/common/scripts/set_path_in_container.sh
-
-bash -c "/bin/bash"
