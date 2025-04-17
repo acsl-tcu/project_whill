df2e3feb2e4b456051b0a424a025a64e6ea6c564
RyonKsystem <tcusconl2020r@gmail.com>
2025-04-17 18:11:32 +0900

    CR2も修正

--- 1_launcher/launch_whill.sh
+++ 1_launcher/launch_whill.sh
@@ -7,5 +7,5 @@ else
   #  cp -p /common/ros_launcher/launch_rplidar/rplidar_s1_bos_launch.py /root/ros2_ws/src/rplidar_ros/launch/
   
   # $(echo "exec ros2 run whill_driver whill --log-opt max-size=100m --log-opt max-file=10 --ros-args --remap __node:=whill_node --remap __ns:=/$HOSTNAME")
-  $(echo "exec ros2 run whill_driver whill --log-opt max-size=100m --log-opt max-file=10 --ros-args --remap __node:=whill_node --remap __ns:=/$HOSTNAME --remap __whmodel:="CR"")
+  $(echo "exec ros2 run whill_driver whill --log-opt max-size=100m --log-opt max-file=10 --ros-args --remap __node:=whill_node --remap __ns:=/$HOSTNAME -p whmodel:="CR"")
 fi
