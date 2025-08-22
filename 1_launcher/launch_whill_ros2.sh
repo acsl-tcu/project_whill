7430960f048be09e83f299b4b3c3b3d71ad3659a
kasekiguchi <kasekiguchi@gmail.com>
2025-08-23 01:08:55 +0900

    Update launch_whill_ros2.sh

--- 1_launcher/launch_whill_ros2.sh
+++ 1_launcher/launch_whill_ros2.sh
@@ -1,10 +1,4 @@
 #! /usr/bin/bash
-  #$(echo "exec 
-  # source /root/.bashrc
-  #")
-#   source /root/.bashrc
-#   echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(ros2 pkg prefix electric_wheelchair)/share/electric_wheelchair/models:$(ros2 pkg prefix create_world)/share/create_world/models" >> ~/.bashrc
-# bash
  ros2 launch connection_wheelchair connection_wheelchair.launch.py 
   #--remap __ns:=/$HOSTNAME
  
