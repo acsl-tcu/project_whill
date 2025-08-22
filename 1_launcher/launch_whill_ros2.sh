ddd5a48c39f5566b69f5e741a2d212608c7c129b
kasekiguchi <kasekiguchi@gmail.com>
2025-08-22 21:31:29 +0900

    Update launch_whill_ros2.sh

--- 1_launcher/launch_whill_ros2.sh
+++ 1_launcher/launch_whill_ros2.sh
@@ -1,7 +1,2 @@
 #! /usr/bin/bash
-if [[ ! "${TAG}" == image_* ]]; then
-  echo "Build first"
-  bash
-else
   $(echo "exec ros2 launch connection_wheelchair connection_wheelchair.launch.py --remap __ns:=/$HOSTNAME")
-fi
