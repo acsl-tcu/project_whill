b3af52b4a2cc6a3f0968d979c1aa4df3c1bfc1b5
kasekiguchi <kasekiguchi@gmail.com>
2025-08-23 00:05:06 +0900

    Update launch_whill_ros2.sh

--- 1_launcher/launch_whill_ros2.sh
+++ 1_launcher/launch_whill_ros2.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
   #$(echo "exec 
-  source /root/.bashrc
-  ros2 launch connection_wheelchair connection_wheelchair.launch.py 
+  # source /root/.bashrc
+  # ros2 launch connection_wheelchair connection_wheelchair.launch.py 
   #--remap __ns:=/$HOSTNAME
   #")
+  bash
