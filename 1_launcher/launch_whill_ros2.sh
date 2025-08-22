7c62ec8d2edf66aaf28d9240eb5c24b5cecb4ab9
kasekiguchi <kasekiguchi@gmail.com>
2025-08-23 00:55:29 +0900

    whill

--- 1_launcher/launch_whill_ros2.sh
+++ 1_launcher/launch_whill_ros2.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/bash
+  #$(echo "exec 
+  # source /root/.bashrc
+  #")
   source /root/.bashrc
-  echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:\
-$(ros2 pkg prefix electric_wheelchair)/share/electric_wheelchair/models:\
-$(ros2 pkg prefix create_world)/share/create_world/models" >> ~/.bashrc \
+  echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(ros2 pkg prefix electric_wheelchair)/share/electric_wheelchair/models:$(ros2 pkg prefix create_world)/share/create_world/models" >> ~/.bashrc
+
   ros2 launch connection_wheelchair connection_wheelchair.launch.py 
   #--remap __ns:=/$HOSTNAME
  
--- project_launch.sh
+++ project_launch.sh
@@ -4,6 +4,6 @@ echo $PROJECT
 echo $ROS_DOMAIN_ID
 
 cd $ACSL_ROS2_DIR/0_host_commands/scripts/
-dup rtk_gnss
+#dup rtk_gnss
 RMW=cyclonedds dup whill_ros2
-dup velodyne run
+#dup velodyne run
