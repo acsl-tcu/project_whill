8dbe1a48e174ff37347b25cedd9275c638bb279b
kasekiguchi <kasekiguchi@gmail.com>
2025-08-28 22:08:23 +0900

    exp

--- project_launch_exp.sh
+++ project_launch_exp.sh
@@ -5,5 +5,5 @@ echo $ROS_DOMAIN_ID
 
 cd $ACSL_ROS2_DIR/0_host_commands/scripts/
 RMW=cyclonedds dup rtk_gnss
-RMW=cyclonedds dup whill
+RMW=cyclonedds dup whill_exp
 RMW=cyclonedds dup velodyne run
