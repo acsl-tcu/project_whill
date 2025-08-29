827c65e7837c07373a5a52994c7b160130103d5f
kasekiguchi <kasekiguchi@gmail.com>
2025-08-29 12:12:27 +0900

    Update launch_whill_sim.sh

--- 1_launcher/launch_whill_sim.sh
+++ 1_launcher/launch_whill_sim.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/bash
- ros2 launch connection_wheelchair connection_wheelchair.launch.py 
+source /root/.bashrc
+ros2 launch connection_wheelchair connection_wheelchair.launch.py 
   #--remap __ns:=/$HOSTNAME
  
