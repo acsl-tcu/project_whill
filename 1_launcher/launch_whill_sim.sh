f8158c02f91fbef8b59f972c9e9436888afbc123
kasekiguchi <kasekiguchi@gmail.com>
2025-08-29 12:33:10 +0900

    Update launch_whill_sim.sh

--- 1_launcher/launch_whill_sim.sh
+++ 1_launcher/launch_whill_sim.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/bash
+sleep 3
 source /root/.bashrc
 printenv | grep GAZEBO
+cat /root/.bashrc
+sleep 2
 ros2 launch connection_wheelchair connection_wheelchair.launch.py 
   #--remap __ns:=/$HOSTNAME
  
