8f55769daa382f113eda85f13a729b79cae215c8
mimoto <166775120+g2481008@users.noreply.github.com>
2025-09-17 14:43:24 +0900

    Update project_launch_exp.sh

--- project_launch_exp.sh
+++ project_launch_exp.sh
@@ -4,7 +4,7 @@ echo $PROJECT
 echo $ROS_DOMAIN_ID
 
 cd $ACSL_ROS2_DIR/0_host_commands/scripts/
-RMW=cyclonedds dup rtk_gnss ublox_launch rtk_gnss rtk_gnss
+RMW=cyclonedds dup rtk_gnss ublox_launch
 RMW=cyclonedds dup whill_exp
 RMW=cyclonedds dup velodyne run
 RMW=cyclonedds dup rtk_gnss run
